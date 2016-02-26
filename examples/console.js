670b2980182e40634e33820c4d6811193d8c78ba
taoyuan <torworx@gmail.com>
2016-02-27 00:55:09 +0800

    0.0.1

--- examples/console.js
+++ examples/console.js
@@ -5,4 +5,4 @@ logu.cli('logu', {colorize: true, timestamp: 'short', showLevel: false, showLabe
 logu.log('info', 'Hello, this is a logging event with host', {'foo': 'bar'});
 logu.log('info', 'action', 'Hello, this is a logging event with host', {'foo': 'bar'});
 logu.log('info', 'build', 'Hello, this is a logging event with host', {'foo': 'bar', 'label': 'logu#*', arr: [1, 2]});
-logu.info({'foo': 'bar'});
+logu.info('say', 'hello', {'foo': 'bar'});
